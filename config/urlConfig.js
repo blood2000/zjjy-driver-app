f2a28af69f4737a23db94454149940e13ad64941
chenkc <501617031@qq.com>
2021-12-10 09:36:32 +0800

    234

--- config/urlConfig.js
+++ config/urlConfig.js
@@ -1,9 +1,9 @@
 const urlConfig = {
 	// BASE_URL: 'http://192.168.1.18:8080',  //强强
 	// BASE_URL: 'http://192.168.30.68:8080',  //孙东
-	BASE_URL: 'http://192.168.30.62:8080',  //萌旗
+	// BASE_URL: 'http://192.168.30.62:8080',  //萌旗
 	// BASE_URL: 'http://10.0.0.66:8080',   //测试服/务器
-	// BASE_URL: 'https://api.chaohaoyun.cn',   //正式地址
+	BASE_URL: 'https://api.chaohaoyun.cn',   //正式地址
 	// BASE_URL: 'http://124.71.47.217:8080'
 	// BASE_URL: 'https://api.zjz1.net'
 }
