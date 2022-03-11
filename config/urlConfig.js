9c56e0dbc1d36fd6cffba92dcf09b2fcfeb0be80
wuyp <wuyuanping2000@outlook.com>
2022-03-11 11:41:56 +0800

    03-11

--- config/urlConfig.js
+++ config/urlConfig.js
@@ -3,8 +3,8 @@ const urlConfig = {
 	// BASE_URL: 'http://192.168.30.68:8080',  //孙东
 	// BASE_URL: 'http://192.168.30.62:8080',  //萌旗
 	// BASE_URL: 'http://192.168.30.71.8080',  //宇宏
-	// BASE_URL: 'http://10.0.0.75:8080',   //测试服/务器
-	BASE_URL: 'https://api.chaohaoyun.cn',   //正式地址
+	BASE_URL: 'http://10.0.0.75:8080',   //测试服/务器
+	// BASE_URL: 'https://api.chaohaoyun.cn',   //正式地址
 	// BASE_URL: 'http://124.71.47.217:8080'
 	// BASE_URL: 'https://api.zjz1.net'
 }
